87668208d8bac74a3f6e5d4c789bdc9a07a8d65d
Darien Quinn Morrison <dmorr041@fiu.edu>
2022-01-29 14:12:12 -0600

    Added COPY and WORKDIR commands to Dockerfile
    Copied over the build context files (my index.js simple web server) and the package.json needed to run our npm install and npm start commands. Also added WORKDIR, so we aren't installing from the container's root, and possibly conflicting with existing dirnames

--- simple-web/index.js
+++ simple-web/index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+// The PORT below is the CONTAINER'S PORT, not your localhost port
 app.listen(8080, () => {
   console.log('App running on port 8080');
 });
