a3d94f6882c18851ae624e50aac1e6a530fa66ac
Darien Quinn Morrison <dmorr041@fiu.edu>
2022-01-29 14:28:31 -0600

    Added additional copies
    Added package.json COPY before npm install command, and copy filesystem after npm install so that any rebuilds of the image always use the cached version of npm install, and the only non cached version is when src/ changes are made

--- simple-web/index.js
+++ simple-web/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('Hello dude');
 });
 
 // The PORT below is the CONTAINER'S PORT, not your localhost port
